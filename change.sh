4fef741479b03f16f60bef09756dccae9a529095
Inaki Villar <inaki.seri@gmail.com>
2025-09-17 14:06:26 -0700

    a

--- change.sh
+++ change.sh
@@ -77,7 +77,7 @@ for ((i=1; i<=ITERATIONS; i++)); do
   echo "===== CYCLE $i ====="
   echo ">>> Performing change: add new private function inside $CLASS_NAME"
   add_private_function "$i"
-  run_build abi_gradle_8_14_3
+  run_build abi_gradle_8_9
 
 done
 
